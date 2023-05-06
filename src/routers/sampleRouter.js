c84a7e85579e7269fba305cfef235d34ba3e7e89
gameitthoi <lamdat3302@gmail.com>
2023-05-06 20:08:15 +0700

    xoa samples

--- src/routers/sampleRouter.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-const router=express.Router()
-import * as sampleController from '../controller/sampleController.js'
-
-router.get("/sample",sampleController.getSample)
-
-export default router;
